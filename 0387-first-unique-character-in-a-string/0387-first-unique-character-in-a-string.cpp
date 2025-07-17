4da7065d13bae8e4c07fb84c6f553e57ba77e783
Pranjal Agrawal <139432325+Pranjal340@users.noreply.github.com>
2025-07-18 00:45:37 +0530

    Time: 0 ms (100%), Space: 14.7 MB (77.56%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,15 +1,15 @@
 class Solution {
- public:
-  int firstUniqChar(string s) {
-    vector<int> count(26);
-
-    for (const char c : s)
-      ++count[c - 'a'];
-
-    for (int i = 0; i < s.length(); ++i)
-      if (count[s[i] - 'a'] == 1)
-        return i;
-
-    return -1;
-  }
+public:
+    int firstUniqChar(string s) {
+        vector<int> v(26,0);
+        for(int i = 0; i < s.length(); i++){
+            v[s[i] - 'a']++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(v[s[i] - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;        
+    }
 };
