e41f52db2e755d93bf007434a08a0f31c172a38f
Pranjal Agrawal <139432325+Pranjal340@users.noreply.github.com>
2025-07-20 23:07:52 +0530

    Time: 47 ms (54.88%), Space: 93.9 MB (88.06%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals-sortingByEndingValues.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    static bool compare(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int rem = 0;
+        sort(intervals.begin(), intervals.end(), compare);
+        int end = intervals[0][1];
+
+        for(int i = 1; i < intervals.size(); i++){
+            if(end > intervals[i][0]){
+                rem++;
+                // end = min(intervals[i][1], end);
+            }
+            else{
+                end = intervals[i][1];
+            }
+        }
+        return rem;        
+    }
+};
