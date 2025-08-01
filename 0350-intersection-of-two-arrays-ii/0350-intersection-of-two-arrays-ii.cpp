62d1f2f330f510e25064ddf08f9763a0f5e81cda
Pranjal Agrawal <139432325+Pranjal340@users.noreply.github.com>
2025-08-01 23:51:50 +0530

    Time: 7 ms (5.52%), Space: 14.7 MB (40.41%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
- public:
-  vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-    if (nums1.size() > nums2.size())
-      return intersect(nums2, nums1);
-
-    vector<int> ans;
-    unordered_map<int, int> count;
-
-    for (const int num : nums1)
-      ++count[num];
-
-    for (const int num : nums2)
-      if (const auto it = count.find(num);
-          it != count.cend() && it->second-- > 0)
-        ans.push_back(num);
-
-    return ans;
-  }
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int>count;
+        
+        vector<int> res;
+        // Count frequency of each number in nums1
+        for(int num : nums1){
+            count[num]++;
+        }
+        
+        for(int i = 0 ; i< nums2.size(); i++){
+            if(count[nums2[i]] > 0){
+                res.push_back(nums2[i]);
+                count[nums2[i]]--;
+            }
+        }
+        return res;         
+    }
 };
