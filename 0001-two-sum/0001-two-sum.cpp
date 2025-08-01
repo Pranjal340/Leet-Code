2500549d8127a881538ab3c381e8393171a774db
Pranjal Agrawal <139432325+Pranjal340@users.noreply.github.com>
2025-08-01 18:02:09 +0530

    Time: 0 ms (100%), Space: 14.8 MB (36.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
- public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> numToIndex;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(hash.find(complement) != hash.end()){
+                return {hash[complement], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
 
-    for (int i = 0; i < nums.size(); ++i) {
-      if (const auto it = numToIndex.find(target - nums[i]);
-          it != numToIndex.cend())
-        return {it->second, i};
-      numToIndex[nums[i]] = i;
     }
-
-    throw;
-  }
 };
