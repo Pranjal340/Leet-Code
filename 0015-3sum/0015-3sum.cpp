1117a9d0d48b7799a6388c482675ca31bb48a082
Pranjal Agrawal <139432325+Pranjal340@users.noreply.github.com>
2025-08-01 22:14:15 +0530

    Time: 48 ms (61.58%), Space: 28.9 MB (90.1%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,40 @@
 class Solution {
- public:
-  vector<vector<int>> threeSum(vector<int>& nums) {
-    if (nums.size() < 3)
-      return {};
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      // Dont use hash Table as cant handle Duplicates
+        vector<vector<int>> ans;
 
-    vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
 
-    ranges::sort(nums);
+        for(int i = 0; i < nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.size() - 1;
 
-    for (int i = 0; i + 2 < nums.size(); ++i) {
-      if (i > 0 && nums[i] == nums[i - 1])
-        continue;
-      // Choose nums[i] as the first number in the triplet, then search the
-      // remaining numbers in [i + 1, n - 1].
-      int l = i + 1;
-      int r = nums.size() - 1;
-      while (l < r) {
-        const int sum = nums[i] + nums[l] + nums[r];
-        if (sum == 0) {
-          ans.push_back({nums[i], nums[l++], nums[r--]});
-          while (l < r && nums[l] == nums[l - 1])
-            ++l;
-          while (l < r && nums[r] == nums[r + 1])
-            --r;
-        } else if (sum < 0) {
-          ++l;
-        } else {
-          --r;
+            while(j < k){
+                int total = nums[i] + nums[j] + nums[k];
+
+                if(total < 0){
+                    j++;
+                }
+                else if(total > 0){
+                    k--;
+                }
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+
+                    j++;
+                    k--;
+
+                }
+            }
         }
-      }
+        return ans;
     }
-
-    return ans;
-  }
 };
